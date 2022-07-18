60fcf0ccea90977958d372540bc4dbd834395fa3
Raj2ietLko <95291812+Raj2ietLko@users.noreply.github.com>
2022-07-18 11:43:18 +0530

    Update 4DecBin.c

--- lab5/4DecBin.c
+++ lab5/4DecBin.c
@@ -4,7 +4,7 @@
 
 int DecBin(int N){
     if(!N)return 0;
-    return DecBin(N>>1)*10+N&2;
+    return DecBin(N>>1)*10+N&1;
     //return DecBin(N/2)*10 + N%2;  //this line is also same as above
 }
 
