3e69ac60c84c5a281540cdac30efabdbdde9381d
Raj2ietLko <95291812+Raj2ietLko@users.noreply.github.com>
2022-08-09 12:33:39 +0530

    Update 3primeFilter.c

--- lab9/3primeFilter.c
+++ lab9/3primeFilter.c
@@ -1,16 +1,18 @@
 // Write a program to print all the prime number, between 1 to 100 in file prime.txt.
 
 #include <stdio.h>
-#include <math.h>
 
 int main(){
     FILE* prime;
     prime=fopen("prime.txt","w");
-    int pr[100],n=0;
-    pr[0]=2;
+    fprintf(prime,"2\n");
     for(int i=3;i<=100;i++){
         int flag=1;
-        for(int j=0;j<min(n,sqrt(i));j++)i%pr[j]==0?(flag=0):(1);
-        if(flag)fprintf("%d\n",i);
+        for(int j=2;j<=i/2;j++){
+            if(i%j==0){flag=0;break;}
+        }
+        if(flag)fprintf(prime,"%d\n",i);
     }
+    fclose(prime);
+    printf("succ");
 }
