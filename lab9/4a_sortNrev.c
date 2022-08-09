ed7247f721c2bad4995307e26588ef98f2616abd
Raj2ietLko <95291812+Raj2ietLko@users.noreply.github.com>
2022-08-09 14:01:54 +0530

    Update 4a_sortNrev.c

--- lab9/4a_sortNrev.c
+++ lab9/4a_sortNrev.c
@@ -10,15 +10,15 @@ int main(){
     scanf("%d",&n);
     List=(int *)malloc(n*sizeof(int));
     printf("enter numbers :");
-    for(int i=0;i<n;i++)scanf("%d",&List[i]);
+    for(int i=0;i<n;i++)scanf("%d",List+i);
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
-            if(List[j]>List[j+1]){
-                t=List[j],List[j]=List[j+1],List[j+1]=t;
+            if( *(List+j) > *(List+j+1) ){
+                t=*(List+j),*(List+j)=*(List+j+1),*(List+j+1)=t;
             }
         }
     }
     for(int i=0;i<n;i++){
-        printf("%d ",List[i]);
+        printf("%d ",*(List+i));
     }
 }
